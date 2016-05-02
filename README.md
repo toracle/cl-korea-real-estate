5dca12ece3ae7230c0fb2b6faa86b2e4858eb836
woori <whdnfl21@gmail.com>
2016-05-02 19:12:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Korea Real Estate Information Crawler (Common Lisp version)
+
+국토부에 게시된 부동산 실거래가 데이터를 가져오는 웹 크롤러.
+
---
+++ src/api.lisp
@@ -0,0 +1,6 @@
+
+(defvar *rtms-mobile-url* "http://rtmobile.molit.go.kr/app/main.jsp")
+
+(defvar *rtms-mobile-base-url* "http://rtmobile.molit.go.kr/mobile.do")
+
+(defvar *rtms-url* "http://rt.molit.go.kr/rtApt.do?cmd=srhLocalView")
