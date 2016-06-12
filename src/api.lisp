66d4e6a8aaace3314c72d044f5ffd028d9b3bbe0
toracle <toracle@gmail.com>
2016-06-12 22:33:24 +0900

    use &rest parameter option when call api/url, api/request, etc

--- src/api.lisp
+++ src/api.lisp
@@ -13,14 +13,23 @@
 
 (defvar *rtms-url* "http://rt.molit.go.kr/rtApt.do?cmd=srhLocalView")
 
-(defun api/url (cmd kwargs)
+(defvar *api-cache-enabled* t)
+
+(defun api/url (cmd &rest kwargs)
   (let ((param (utils/url-encode kwargs)))
     (format nil "~a~a.do?~a" *rtms-mobile-base-url* cmd param)))
 
-(defun api/request (cmd kwargs)
-  (let* ((url (api/url cmd kwargs))
+(defun api/request (cmd &rest kwargs)
+  (let* ((url (apply #'api/url (cons cmd kwargs)))
 	 (content (dex:get url)))
     content))
 
-(defun api/request-json (cmd kwargs)
-  (json:decode-json-from-string (api/request cmd kwargs)))
+(defun api/content-provider (cmd &rest kwargs)
+  (if *api-cache-enabled*
+      (if (cache/is-content-cached cmd kwargs)
+	  (cache/get-cached-content kwargs)
+	  (cace/cache-content '(api/request cmd kwargs)))
+      (api/request cmd kwargs)))
+
+(defun api/request-json (cmd &rest kwargs)
+  (json:decode-json-from-string (apply #'api/request (cons cmd kwargs))))
--- src/test.lisp
+++ src/test.lisp
@@ -1,4 +1,10 @@
 (load "api")
 
 (utils/name-code-result-to-alist
- (api/request-json "getGugunListAjax" '(:sidoCode "11")))
+ (api/request-json "getGugunListAjax" :sidoCode "11"))
+
+(api/url "getGugunListAjax" :sidoCode "11")
+
+(api/request "getGugunListAjax" :sidoCode "11")
+
+(api/request-json "getGugunListAjax" :sidoCode "11")
